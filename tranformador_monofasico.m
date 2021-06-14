c9b8189f2d91fcb0c0e726acbd346c294bb32480
emanuel-alves <emanuel.alves@unifei.edu.br>
2021-06-14 13:54:59 -0300

    add latex, salve directory

---
+++ tranformador_monofasico.m
@@ -0,0 +1,4 @@
+function [saidas] = tranformador_monofasico(SN,VNAT,VNBT,V0,I0,P0,Vcc,Icc,PCC)
+x = -100:0; plot(x, x.^2)
+end
+
---
+++ tranformador_trifasico.m
@@ -0,0 +1,4 @@
+function [saidas] = tranformador_trifasico(SN,VNAT,VNBT,V0,I0,P0,Vcc,Icc,PCC)
+x = 0:100; plot(x, x.^2)
+end
+
