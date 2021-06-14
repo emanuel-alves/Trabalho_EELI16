f9f0c078391e8751ae282d609b9cd5d82d6b2de2
emanuel-alves <emanuel.alves@unifei.edu.br>
2021-06-14 20:29:52 -0300

    opção salvar finalizado

--- tranformador_monofasico.m
+++ tranformador_monofasico.m
@@ -1,4 +1,277 @@
-function [saidas] = tranformador_monofasico(SN,VNAT,VNBT,V0,I0,P0,Vcc,Icc,PCC)
-x = -100:0; plot(x, x.^2)
-end
+function [saidas] = tranformador_monofasico(SN_1,VNAT_1,VNBT_1,V0_1,I0_1,P0_1,Vcc_1,Icc_1,Pcc_1, isPlot, isSalve)
+    T_1 = 75 % nao dado
 
+    if isSalve
+        pathSave = uigetdir('','Selecione o diretório para salvar os dados');
+    end
+% Topico 2 %
+%Figura 1%
+
+RM_1 = (V0_1^2)/P0_1
+ZM_1 = V0_1/I0_1
+I0P_1 = V0_1/RM_1
+I0Q_1 = sqrt(I0_1^2 - I0P_1^2)
+XM_1 = V0_1/I0Q_1
+
+R1S_1 = (Pcc_1/Icc_1^2)
+Z1S_1 = (Vcc_1/Icc_1)
+X1S_1 = (sqrt(Z1S_1^2 - R1S_1^2))
+R1TS_1 = (235 + T_1)/(235 + 25)*R1S_1
+Z1TS_1 = sqrt(R1TS_1^2 + X1S_1^2)
+
+R2A_1 = R1TS_1/2
+X2A_1 = X1S_1/2
+R1_1 = R1TS_1/2
+X1_1 = X1S_1/2
+
+a = (VNAT_1/VNBT_1)
+
+% Referindo ao lado de alta
+RMA_1 = a^2*RM_1
+XMA_1 = a^2*XM_1
+
+% Referindo ao lado de baixa
+R1B_1 = R1_1/a^2
+X1B_1 = X1_1/a^2
+R2_1 = R2A_1/a^2
+X2_1 = X2A_1/a^2
+
+%Figura 2%
+
+% Referindo ao lado de alta
+RMA_1
+XMA_1
+R1TS_1
+X1S_1
+
+% Referindo ao lado de baixa
+RM_1
+XM_1
+R1SB_1 = R1TS_1/a^2
+X1SB_1 = X1S_1/a^2
+
+%Figura 3%
+
+% Referindo ao lado de alta
+R1TS_1
+X1S_1
+
+% Referindo ao lado de baixa
+R1SB_1
+X1SB_1
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Topico 3 %
+%Figura 1%
+
+RMPU_1 = SN_1/P0_1
+RMPC_1 = RMPU_1*100
+Q0_1 = sqrt((V0_1*I0_1)^2 - P0_1^2)
+XMPU_1 = SN_1/Q0_1
+XMPC_1 = XMPU_1*100
+
+R1SPU_1 = Pcc_1/SN_1
+R1SPC_1 = R1SPU_1*100
+Z1SPU_1 = Vcc_1/VNAT_1
+Z1SPC_1 = Z1SPU_1*100
+X1SPU_1 = sqrt(Z1SPU_1^2 - R1SPU_1^2)
+X1SPC_1 = X1SPU_1*100
+R1STPU_1 = (235 + T_1)/(235 + 25)*R1SPU_1
+R1STPC_1 = R1STPU_1*100
+Z1STPU_1 = sqrt(R1STPU_1^2 + X1SPU_1^2)
+Z1STPC_1 = Z1STPU_1*100
+
+R2APU_1 = R1STPU_1/2
+X2APU_1 = X1SPU_1/2
+R1PU_1 = R1STPU_1/2
+X1PU_1 = X1SPU_1/2
+
+
+%Figura 2%
+
+R1STPU_1
+X1SPU_1
+RMPU_1
+XMPU_1
+
+%Figura 3%
+
+R1STPU_1
+X1SPU_1
+
+
+% Comparando com o topico b
+
+RMPUB_1 = RM_1/(VNBT_1^2/SN_1)
+RMPU_1
+XMPUB_1 = XM_1/(VNBT_1^2/SN_1)
+XMPU_1
+R1TSPUB_1 = R1TS_1/(VNAT_1^2/SN_1)
+R1STPU_1
+X1SPUB_1 = X1S_1/(VNAT_1^2/SN_1)
+X1SPU_1
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Topico 4 %
+
+fp = [0.8 0.85 0.9 0.95]
+
+phi = acos(fp)
+
+IPU_1 = 0:0.05:1
+
+RegPU_1_1 = IPU_1.*(cos(phi(1))*R1STPU_1 + sin(phi(1))*X1SPU_1)
+RegPU_1_2 = IPU_1.*(cos(phi(2))*R1STPU_1 + sin(phi(2))*X1SPU_1)
+RegPU_1_3 = IPU_1.*(cos(phi(3))*R1STPU_1 + sin(phi(3))*X1SPU_1)
+RegPU_1_4 = IPU_1.*(cos(phi(4))*R1STPU_1 + sin(phi(4))*X1SPU_1)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Topico 5 %
+
+fp = [0.8 0.85 0.9 0.95]
+
+phi = acos(fp)
+
+IPU_1 = 0:0.05:1
+
+
+RegPU_1_1 = IPU_1.*(cos(phi(1))*R1STPU_1 - sin(phi(1))*X1SPU_1*100)
+RegPU_1_2 = IPU_1.*(cos(phi(2))*R1STPU_1 - sin(phi(2))*X1SPU_1*100)
+RegPU_1_3 = IPU_1.*(cos(phi(3))*R1STPU_1 - sin(phi(3))*X1SPU_1*100)
+RegPU_1_4 = IPU_1.*(cos(phi(4))*R1STPU_1 - sin(phi(4))*X1SPU_1*100)
+
+
+amps = 0:0.05:1; % Valores de corrente(A)[pu]
+
+% Capacitiva (-)
+
+I(1,:) = amps.*(0.8*R1STPU_1 - X1SPU_1*sind(acosd(0.8))*100);
+I(2,:) = amps.*(0.85*R1STPU_1 - X1SPU_1*sind(acosd(0.85))*100);
+I(3,:) = amps.*(0.9*R1STPU_1 - X1SPU_1*sind(acosd(0.9))*100);
+I(4,:) = amps.*(0.95*R1STPU_1 - X1SPU_1*sind(acosd(0.95))*100);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Topico 6 %
+
+fp = [0.8 0.85 0.9 0.95]
+
+phi = acos(fp)
+
+IPU_1 = 0:0.05:1
+
+RegPU_1_1 = IPU_1.*R1STPU_1
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Graficos%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if isPlot
+    figure('units','normalized','outerposition',[0 0 1 1]);
+    
+    %%% Topico 4
+    subplot(2,2,1)
+    plot(IPU_1,RegPU_1_1,IPU_1,RegPU_1_2,IPU_1,RegPU_1_3,IPU_1,RegPU_1_4)
+    grid on
+    title('Regula��o de tens�o | Carga indutiva-resistiva')
+    ylabel('Reg_{PU}')
+    xlabel('I_{PU}')
+    legend('fp = 0.8','fp = 0.85','fp = 0.9','fp = 0.95')
+    
+    %%% Topico 5
+    
+    subplot(2,2,2)
+    plot(IPU_1,RegPU_1_1,IPU_1,RegPU_1_2,IPU_1,RegPU_1_3,IPU_1,RegPU_1_4)
+    grid on
+    title('Regula��o de tens�o | Carga capacitiva-resistiva')
+    ylabel('Reg_{PU}')
+    xlabel('I_{PU}')
+    legend('fp = 0.8','fp = 0.85','fp = 0.9','fp = 0.95')
+    
+    %%%
+    
+    % Plotagem da regula��o de tens�o
+    subplot(2,2,3)
+    %figure('Name','Regula��o de tensao para carregamentos resistivo-capacitivos','NumberTitle','off','Position',[500 100 800 600])
+    plot(amps,I(1,:),'b');
+    hold on;
+    plot(amps,I(2,:),'k');
+    plot(amps,I(3,:),'r');
+    plot(amps,I(4,:),'m');
+    
+    grid on;
+    title ('Regula��o de Tens�o Versus Carga (resistivo-capacitivo)');
+    xlabel ('Caregamento(A)');
+    ylabel ('Regula��o de Tens�o (%)');
+    legend({'FP 0,8 adiantado','FP 0,85 adiantado','FP 0,9 adiantado', 'FP 0,95 adiantado'},'Location','best');
+    hold off;
+    
+    
+    %%% Topico 6
+    
+    subplot(2,2,4)
+    plot(IPU_1,RegPU_1_1)
+    grid on
+    title('Regula��o de tens�o | Carga resistiva')
+    ylabel('Reg_{PU}')
+    xlabel('I_{PU}')
+    legend('fp = 1')
+
+
+end
+if isSalve
+    f = figure('visible','off');
+    plot(IPU_1,RegPU_1_1,IPU_1,RegPU_1_2,IPU_1,RegPU_1_3,IPU_1,RegPU_1_4)
+    grid on
+    title('Regula��o de tens�o | Carga indutiva-resistiva')
+    ylabel('Reg_{PU}')
+    xlabel('I_{PU}')
+    legend('fp = 0.8','fp = 0.85','fp = 0.9','fp = 0.95')
+    saveas(f,  fullfile(pathSave, 'plot_1'),'png')
+
+    %%% Topico 5
+    
+    f = figure('visible','off');
+    plot(IPU_1,RegPU_1_1,IPU_1,RegPU_1_2,IPU_1,RegPU_1_3,IPU_1,RegPU_1_4)
+    grid on
+    title('Regula��o de tens�o | Carga capacitiva-resistiva')
+    ylabel('Reg_{PU}')
+    xlabel('I_{PU}')
+    legend('fp = 0.8','fp = 0.85','fp = 0.9','fp = 0.95')
+    saveas(f,  fullfile(pathSave, 'plot_2'),'png')
+
+
+    %%%
+    
+    % Plotagem da regula��o de tens�o
+    f = figure('visible','off');
+    %figure('Name','Regula��o de tensao para carregamentos resistivo-capacitivos','NumberTitle','off','Position',[500 100 800 600])
+    plot(amps,I(1,:),'b');
+    hold on;
+    plot(amps,I(2,:),'k');
+    plot(amps,I(3,:),'r');
+    plot(amps,I(4,:),'m');
+    
+    grid on;
+    title ('Regula��o de Tens�o Versus Carga (resistivo-capacitivo)');
+    xlabel ('Caregamento(A)');
+    ylabel ('Regula��o de Tens�o (%)');
+    legend({'FP 0,8 adiantado','FP 0,85 adiantado','FP 0,9 adiantado', 'FP 0,95 adiantado'},'Location','best');
+    hold off;
+    saveas(f,  fullfile(pathSave, 'plot_3'),'png')
+
+
+    
+    %%% Topico 6
+    
+    
+    f = figure('visible','off');
+    plot(IPU_1,RegPU_1_1)
+    grid on
+    title('Regula��o de tens�o | Carga resistiva')
+    ylabel('Reg_{PU}')
+    xlabel('I_{PU}')
+    legend('fp = 1')
+    saveas(f,  fullfile(pathSave, 'plot_4'),'png')
+
+
+end
+end
--- tranformador_trifasico.m
+++ tranformador_trifasico.m
@@ -1,4 +1,114 @@
-function [saidas] = tranformador_trifasico(SN,VNAT,VNBT,V0,I0,P0,Vcc,Icc,PCC)
-x = 0:100; plot(x, x.^2)
+function [saidas] = tranformador_trifasico(SN_3, VNBT_3, VNBT_3, V0_3, I0_3, P0_3, Vcc_3, Icc_3, Pcc_3,  isPlot, isSalve, pathSave)
+
+T_3 = 75 % nao dado
+
+% Topico 2 %
+%Figura 1%
+
+RM_3 = (V0_3^2)/P0_3
+I0P_3 = (V0_3/sqrt(3))/RM_3
+I0Q_3 = sqrt(I0_3^2 - I0P_3^2)
+XM_3 = (V0_3/sqrt(3))/I0Q_3
+%ZM_3 = (V0_3/sqrt(3))/I0_1
+
+Z1S_3 = (Vcc_3/sqrt(3))/Icc_3
+R1S_3 = (Pcc_3/3)/Icc_3^2
+X1S_3 = (sqrt(Z1S_3^2 - R1S_3^2))
+R1TS_3 = (235 + T_3)/(235 + 25)*R1S_3
+Z1TS_3 = sqrt(R1TS_3^2 + X1S_3^2)
+
+R2A_3 = R1TS_3/2
+X2A_3 = X1S_3/2
+R1_3 = R1TS_3/2
+X1_3 = X1S_3/2
+
+a = (VNAT_3/VNBT_3)
+
+% Referindo ao lado de alta
+RMA_3 = a^2*RM_3
+XMA_3 = a^2*XM_3
+
+% Referindo ao lado de baixa
+R1B_3 = R1_3/a^2
+X1B_3 = X1_3/a^2
+R2_3 = R2A_3/a^2
+X2_3 = X2A_3/a^2
+
+% Referindo ao lado de alta
+RMA_3
+XMA_3
+R1S_3
+X1S_3
+
+% Referindo ao lado de baixa
+RM_3
+XM_3
+R1SB_3 = R1S_3/a^2
+X1SB_3 = X1S_3/a^2
+
+%Figura 3%
+
+% Referindo ao lado de alta
+R1S_3
+X1S_3
+
+% Referindo ao lado de baixa
+R1SB_3
+X1SB_3
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Topico 3 %
+%Figura 1%
+
+RMPU_3 = SN_3/P0_3
+RMPC_3 = RMPU_3*100
+Q0_3 = sqrt((sqrt(3)*V0_3*I0_3)^2 - P0_3^2)
+XMPU_3 = SN_3/Q0_3
+XMPC_3 = XMPU_3*100
+
+R1SPU_3 = Pcc_3/SN_3
+R1SPC_3 = R1SPU_3*100
+Z1SPU_3 = Vcc_3/VNAT_3
+Z1SPC_3 = Z1SPU_3*100
+X1SPU_3 = sqrt(Z1SPU_3^2 - R1SPU_3^2)
+X1SPC_3 = X1SPU_3*100
+R1STPU_3 = (235 + T_3)/(235 + 25)*R1SPU_3
+R1STPC_3 = R1STPU_3*100
+Z1STPU_3 = sqrt(R1STPU_3^2 + X1SPU_3^2)
+Z1STPC_3 = Z1STPU_3*100
+
+%Figura 1%
+
+R2APU_3 = R1STPU_3/2
+X2APU_3 = X1SPU_3/2
+R1PU_3 = R1STPU_3/2
+X1PU_3 = X1SPU_3/2
+RMPU_3
+XMPU_3
+
+%Figura 2%
+
+R1STPU_3
+X1SPU_3
+RMPU_3
+XMPU_3
+
+%Figura 3%
+
+R1STPU_3
+X1SPU_3
+
+% Comparando com o topico b
+fprintf("Comparando")
+RMPUB_3 = RM_3/(((V0_3)^2)/SN_3)
+RMPU_3
+XMPUB_3 = XM_3/(((V0_3)^2)/SN_3)
+XMPU_3
+R1TSPUB_3 = R1TS_3/(VNAT_3^2/SN_3)
+R1STPU_3
+X1SPUB_3 = X1S_3/(VNAT_3^2/SN_3)
+X1SPU_3
+
 end
 
